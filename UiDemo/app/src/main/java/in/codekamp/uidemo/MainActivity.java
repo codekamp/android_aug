deb411abcc95793235df3064e3c171a2a14bba18
Prashant Chaudhary <101.prashant@gmail.com>
2017-08-15 19:18:28 +0530

    Impilicit intent and chooser

--- UiDemo/app/src/main/java/in/codekamp/uidemo/MainActivity.java
+++ UiDemo/app/src/main/java/in/codekamp/uidemo/MainActivity.java
@@ -2,20 +2,26 @@ package in.codekamp.uidemo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText firstField;
     private EditText secondField;
+    private TextView resultField;
+    private int result;
 
     private static final int STATE_PICK_REQUEST = 0;
     private static final int COUNTRY_PICK_REQUEST = 1;
+
+    private static final String RESULT_KEY = "my_result";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +29,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         firstField = (EditText) findViewById(R.id.first_field);
         secondField = (EditText) findViewById(R.id.second_field);
+        resultField = (TextView) findViewById(R.id.result_view);
+
+        if(savedInstanceState != null) {
+            result = savedInstanceState.getInt(RESULT_KEY, 0);
+            resultField.setText(Integer.toString(result));
+        } else {
+            Log.d("codekamp", "activity created for the first time");
+        }
+
+        Log.d("codekamp", "onCreate of MainActivity");
     }
 
     public void addNumbers(View v) {
         Log.d("codekamp", "Button clicked");
         Log.d("codekamp", "First number is " + firstField.getText());
         Log.d("codekamp", "Second number is " + secondField.getText());
+//
+//        int firstNum = Integer.parseInt(firstField.getText().toString());
+//        int secondNum = Integer.parseInt(secondField.getText().toString());
+//        result = firstNum + secondNum;
 
-        int firstNum = Integer.parseInt(firstField.getText().toString());
-        int secondNum = Integer.parseInt(secondField.getText().toString());
-        int result = firstNum + secondNum;
+//        resultField.setText(Integer.toString(result));
 
-        Intent i = new Intent(this, ResultActivity.class);
-        i.putExtra("my_final_result", result);
-        startActivityForResult(i, STATE_PICK_REQUEST);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:prashant@codekamp.in,helpdesk@codekamp.in"));
+        intent.putExtra(Intent.EXTRA_EMAIL, "prashant@codekamp.in");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Hello world subject");
+        intent.putExtra(Intent.EXTRA_TEXT, "This is message body");
+
+        Intent chooserIntent = Intent.createChooser(intent, "Select an app to invite");
+        startActivity(chooserIntent);
     }
 
     @Override
@@ -50,6 +73,50 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-}
 
-//github.com/codekamp
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("codekamp", "onStart of MainActivity");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("codekamp", "onPause of MainActivity");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("codekamp", "onResume of MainActivity");
+    }
+
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("codekamp", "onStop of MainActivity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("codekamp", "onDestroy of MainActivity");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(RESULT_KEY, result);
+
+        Log.d("codekamp", "onSaveInstanceState of MainActivity");
+    }
+}
--- UiDemo/app/src/main/java/in/codekamp/uidemo/ResultActivity.java
+++ UiDemo/app/src/main/java/in/codekamp/uidemo/ResultActivity.java
@@ -1,10 +1,12 @@
 package in.codekamp.uidemo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,6 +19,13 @@ public class ResultActivity extends AppCompatActivity {
 
     public TextView resultView;
     public EditText uselessField;
+    private static final String INTENT_RESULT_KEY = "my_final_result";
+
+    public static Intent createIntent(Context c, int result) {
+        Intent i = new Intent(c, ResultActivity.class);
+        i.putExtra(INTENT_RESULT_KEY, result);
+        return i;
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,10 +37,11 @@ public class ResultActivity extends AppCompatActivity {
         uselessField = (EditText) findViewById(R.id.useless_field);
 
         Intent i = getIntent();
-        int result = i.getIntExtra("my_final_result", 0);
+        int result = i.getIntExtra(INTENT_RESULT_KEY, 0);
 
         resultView.setText(Integer.toString(result));
 
+        Log.d("codekamp", "onCreate of ResultActivity");
     }
 
     public void sendDataBack(View view) {
@@ -41,5 +51,48 @@ public class ResultActivity extends AppCompatActivity {
         setResult(Activity.RESULT_OK, i);
         setResult(Activity.RESULT_CANCELED);
         finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("codekamp", "onStart of ResultActivity");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("codekamp", "onPause of ResultActivity");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("codekamp", "onResume of ResultActivity");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("codekamp", "onStop of ResultActivity");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("codekamp", "onDestroy of ResultActivity");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.d("codekamp", "onSaveInstanceState of ResultActivity");
     }
 }
